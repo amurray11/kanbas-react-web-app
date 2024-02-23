c0151dfbf8857e9506fae356f67b5ad47a82addf
amurray11 <murray.anna@northeastern.edu>
2024-02-23 16:21:19 -0500

    JavaScript and React.js Assignment 3

---
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaCheckCircle, FaEllipsisV, FaPlusCircle } from "react-icons/fa";
+import { Link, useParams } from "react-router-dom";
+import { assignments } from "../../Database";
+function Assignments() {
+  const { courseId } = useParams();
+  const assignmentList = assignments.filter(
+    (assignment) => assignment.course === courseId);
+  return (
+    <>
+      <div style={{ textAlign: "right", paddingRight: "10"}}>
+        <button>Collapse All</button>
+        <button>View Progress</button>
+        <select style={{ width: "100px" }}>
+          <option>Publish All</option>
+          <option>Publish All Modules and Items</option>
+          <option>Publish Modules Only</option>
+          <option>Unpublish All</option>
+        </select>
+        <button className="button-module">+Module</button>
+        <hr></hr>
+      </div>
+      <ul className="list-group wd-modules">
+        <li className="list-group-item">
+          <div>
+            <FaEllipsisV className="me-2" /> ASSIGNMENTS
+            <span className="float-end">
+              <FaCheckCircle className="text-success" />
+              <FaPlusCircle className="ms-2" /><FaEllipsisV className="ms-2" />
+            </span>
+          </div>
+          <ul className="list-group">
+            {assignmentList.map((assignment) => (
+              <li className="list-group-item">
+                <FaEllipsisV className="me-2" />
+                <Link
+                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
+                <span className="float-end">
+                  <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
+              </li>))}
+          </ul>
+        </li>
+      </ul>
+    </>
+);}
+export default Assignments;
---
+++ src/Labs/a3/JavaScript/WorkingWithFunctions.tsx
@@ -0,0 +1,15 @@
+import ArrowFunctions from "./functions/ArrowFunctions";
+import ES5Functions from "./functions/ES5Functions";
+import FunctionParenthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import ImpliedReturn from "./functions/ImpliedReturn";
+function JavaScript() {
+    return(
+       <div>
+          <ES5Functions/>
+          <ArrowFunctions/>
+          <ImpliedReturn/>
+          <FunctionParenthesisAndParameters/>
+       </div>
+    );
+ }
+ export default JavaScript
